49ac0c41711b6d58fe98bcd52ee4400c63174af0
Faseela <faseela8861@gmail.com>
2024-09-10 11:07:27 +0530

    create functions for remove and add task and some updates

--- src/components/Todoapp/Todo.js
+++ src/components/Todoapp/Todo.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddTask from './AddTask';
 import ListTask from './ListTask';
 import "./Todo.css";
 
 const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const addTask =(title)=> {
+        const newTask = [...tasks,{title}]
+        setTasks(newTask);
+  }
+
+  const removeTask = (index)=> {
+    const newTask = [...tasks]
+    newTask.splice(index,1);
+    setTasks(newTask)
+  }
   return (
     <>
       <div className="todo-container">
         <div className="header"> TODO APP</div>
             <div className="add-task">
-                <AddTask />
+                <AddTask addTask={addTask} />
             </div>
              <div className="tasks"> 
-                 <ListTask />
+              {tasks.map((tasks, index)=>
+                  <ListTask task={tasks} removeTask={removeTask} index={index} key={index}/>
+              )}
+               
                  </div>
                   
            
